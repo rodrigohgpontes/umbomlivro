7335f2426c48e9c0ebdfcd96177242c989568fc3
Rodrigo Pontes <rodrigohgpontes@gmail.com>
2020-05-08 20:40:12 -0700

    Continue to add style to home page posts

--- src/components/BlogRollItem.js
+++ src/components/BlogRollItem.js
@@ -32,11 +32,24 @@ class BlogRollItem extends React.Component {
       >
         <div className="post__book">
           <div className="post__book-info">
-            <p className="post__book-title">{postData.frontmatter.booktitle}</p>
-            <p className="post__book-author">
-              {postData.frontmatter.bookauthor}
-            </p>
-            <p className="post__book-year">{postData.frontmatter.bookyear}</p>
+            {postData.frontmatter.bookcover ? (
+              <BackgroundImage
+                Tag="div"
+                className="post__book-cover"
+                fluid={postData.frontmatter.bookcover.childImageSharp.fluid}
+                backgroundColor={`#000`}
+              />
+            ) : null}
+
+            <div className="post__book-details">
+              <p className="post__book-title">
+                {postData.frontmatter.booktitle}
+              </p>
+              <p className="post__book-author">
+                {postData.frontmatter.bookauthor}
+              </p>
+              <p className="post__book-year">{postData.frontmatter.bookyear}</p>
+            </div>
           </div>
 
           <div className="post__book-recommendation">
@@ -50,15 +63,7 @@ class BlogRollItem extends React.Component {
         </div>
 
         <div className="post__images">
-          {postData.frontmatter.guestpicture ? (
-            <BackgroundImage
-              Tag="div"
-              className="post__images-guest"
-              fluid={postData.frontmatter.guestpicture.childImageSharp.fluid}
-              backgroundColor={`#000`}
-            />
-          ) : null}
-
+          <div className="post__images-plus-sign">+</div>
           {postData.frontmatter.bookcover ? (
             <BackgroundImage
               Tag="div"
@@ -67,16 +72,52 @@ class BlogRollItem extends React.Component {
               backgroundColor={`#000`}
             />
           ) : null}
+
+          {postData.frontmatter.guestpicture ? (
+            <BackgroundImage
+              Tag="div"
+              className="post__images-guest"
+              fluid={postData.frontmatter.guestpicture.childImageSharp.fluid}
+              backgroundColor={`#000`}
+            />
+          ) : null}
         </div>
 
         <div className="post__guest">
-          <p className="post__guest-name">{postData.frontmatter.guestname}</p>
-          <p className="post__guest-bio">{postData.frontmatter.guestbio}</p>
+          <div className="post__guest-details">
+            <p className="post__guest-name">{postData.frontmatter.guestname}</p>
+            <p className="post__guest-bio">{postData.frontmatter.guestbio}</p>
+          </div>
 
-          <Link className="post__link" to={postData.fields.slug}>
-            read more
-          </Link>
+          {postData.frontmatter.guestpicture ? (
+            <BackgroundImage
+              Tag="div"
+              className="post__guest-picture"
+              fluid={postData.frontmatter.guestpicture.childImageSharp.fluid}
+              backgroundColor={`#000`}
+            />
+          ) : null}
         </div>
+
+        <Link className="post__link" to={postData.fields.slug}>
+          Leia a entrevista completa{"    "}
+          <svg
+            class="post__link-arrow"
+            width="54"
+            height="15"
+            viewBox="0 0 54 15"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g
+              fill-rule="nonzero"
+              stroke="#a32a99"
+              fill="none"
+              stroke-linecap="square"
+            >
+              <path d="M.5 7h51.919M46 0l7 7.147L46 14"></path>
+            </g>
+          </svg>
+        </Link>
       </article>
     );
   }
